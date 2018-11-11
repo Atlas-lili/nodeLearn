7ad418f404738fee2298c0e85132b88428e2c535
Atlas-lili <32982388+Atlas-lili@users.noreply.github.com>
2018-11-11 23:34:36 +0800

    day04 add

--- day04/express01/index.js
+++ day04/express01/index.js
@@ -1,5 +1,27 @@
 const express = require('express');
 const app = new express();
+
+function findInDB (callback) {
+    const MongoClient = require('mongodb').MongoClient;
+    const dbUrl = 'mongodb://127.0.0.1:27017/mongoTest';
+    MongoClient.connect(dbUrl, { useNewUrlParser: true }, (err,client) => {
+        if(err){
+            console.log(err);
+            return;
+        }
+        const db = client.db('mongoTest');
+        let result = db.collection('user').find({});
+        result.toArray((err, data) => {
+            if(err){
+                console.log(err);
+                return;
+            }
+            console.log(data);
+            callback(data);
+        })
+
+    })
+}
 app.set('view engine', 'ejs');
 app.get('/', (req, res) => res.send('Hello World!'));
 // app.get('/news', (req, res) => {
@@ -10,12 +32,16 @@ app.get('/', (req, res) => res.send('Hello World!'));
 // //     console.log(req.params);
 // //     res.send('Hello News!')
 // // });
-app.get('/news', (req, res) => {
-    res.render('home',{
-        name: 'news',
-        list: [1,2,3,4,5]
-    })
+findInDB((info) => {
+    app.get('/news', (req, res) => {
+        res.render('home',{
+            name: 'news',
+            list: [1,2,3,4,5],
+            list01: info
+        })
+    });
 });
+
 app.get('/shop/:aid', (req, res) => {// 动态路由
 
 });
